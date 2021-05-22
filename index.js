e68f551214506cf9e9f7a07160fea3db36ec5dd4
mihai <mihaimihai011@gmail.com>
2021-05-22 15:44:58 +0300

    Added that can execute stuff, bu only basics, without interactivity

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-var app = new Vue({
-    el: '#example-3',
-    methods: {
-        say: function (message) {
-            alert(message)
-        }
-    }
-})
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const { spawn } = require('child_process');
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('command', (msg) => {
+        console.log('message: ' + msg);
+
+        const com = spawn(msg, [], { shell: true });
+
+        com.stdout.on('data', (data) => {
+            socket.emit('result', data);
+        });
+
+        com.stderr.on('data', (data) => {
+            socket.emit('result', data);
+        });
+
+        com.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
+
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
