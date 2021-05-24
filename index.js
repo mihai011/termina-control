885e3c69c5e0b7157cff3274304b8ddc118fe523
mihai <mihaimihai011@gmail.com>
2021-05-24 22:39:32 +0300

    Added a bit of code to it.

--- index.js
+++ index.js
@@ -5,28 +5,35 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const { spawn } = require('child_process');
+const pty = require('node-pty');
+
+app.use(express.static('public'))
+app.use(express.static('node_modules'))
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+
+    term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
+        name: 'xterm-256color',
+        cols: 80,
+        rows: 24,
+        encoding: 'utf8'
+    });
+
+    term.on('data', function(data) {
+
+		socket.emit('result', data);
+
+	});
+
     socket.on('command', (msg) => {
+
         console.log('message: ' + msg);
-
-        const com = spawn(msg, [], { shell: true });
-
-        com.stdout.on('data', (data) => {
-            socket.emit('result', data);
-        });
-
-        com.stderr.on('data', (data) => {
-            socket.emit('result', data);
-        });
-
-        com.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
-        });
+        term.write(data);
 
     });
 });
